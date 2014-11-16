693539e640c24753fa95aecff46b57a999890348
Eric Adlam <eadlam@gmail.com>
2014-11-16 16:44:44 +1030

    changing settings static dir

--- settings.py
+++ settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = '/home/django/django_project/django_project/static'
+STATIC_ROOT = '/home/ubuntu/personas/static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
