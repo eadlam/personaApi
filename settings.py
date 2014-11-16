2b31ac9e6bd7f906ba45fd0faf54adc7aa77a1aa
Eric Adlam <eadlam@gmail.com>
2014-11-16 17:01:39 +1030

    added static

--- settings.py
+++ settings.py
@@ -91,9 +91,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = '/home/ubuntu/personas/static'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
